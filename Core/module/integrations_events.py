55bde45f9c67d2b6641308e9b779a5e04c992a36
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-12 04:09:53 -0800

    Update integrations_events.py

--- Core/module/integrations_events.py
+++ Core/module/integrations_events.py
@@ -5,7 +5,7 @@
 # PASIVO | SOLO EMITE
 # ============================================================
 
-from emit_event import emit_event
+from infrastructure.events.emit_events import emit_event
 
 def emit_integration_event(event_name: str, payload: dict):
     emit_event("business", {
