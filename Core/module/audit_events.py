7a5ec3cff37811d1e7194aacfdc962bee6aceba2
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-12 04:07:10 -0800

    Update audit_events.py

--- Core/module/audit_events.py
+++ Core/module/audit_events.py
@@ -3,7 +3,7 @@
 # EVENTOS DE AUDITORÍA
 # PASIVO | SOLO EMITE
 
-from emit_event import emit_event
+from infrastructure.events.emit_events import emit_event
 
 def emit_audit_event(event_name: str, payload: dict):
     emit_event("core", {
