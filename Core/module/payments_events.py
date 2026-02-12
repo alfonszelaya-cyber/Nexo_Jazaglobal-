5ae2cafbe0b52417c2c3cb2f6cbf1f06993e7530
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-12 04:21:05 -0800

    Update payments_events.py

--- Core/module/payments_events.py
+++ Core/module/payments_events.py
@@ -5,7 +5,7 @@
 # PASIVO | SOLO EMITE
 # ============================================================
 
-from emit_event import emit_event
+from infrastructure.events.emit_events import emit_events
 
 def emit_payment_event(event_name: str, payload: dict):
     emit_event("business", {
