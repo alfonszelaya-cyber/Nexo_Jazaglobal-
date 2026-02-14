bbbe5751eb8922d92b4ecf246159cdda9861d6ae
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-13 18:53:11 -0800

    Update reports_events.py

--- Core/module/reports_events.py
+++ Core/module/reports_events.py
@@ -5,7 +5,7 @@
 # PASIVO | SOLO EMITE
 # ============================================================
 
-from infrastructure.events.emit_events import emit_events
+from Core.module.emit_events import emit_events
 
 def emit_report_event(event_name: str, payload: dict):
     emit_event("business", {
